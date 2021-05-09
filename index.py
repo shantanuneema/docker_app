4973b157e23c6264a583e9bd22b67b74516950c1
Axita Gupta <axitagupta@Axitas-MacBook-Air.local>
2021-05-08 21:34:42 -0700

    docker connection check

---
+++ index.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+@app.route("/")
+
+def example():
+    return "Let's learn Docker!"#add our message
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=int("5000"), debug=True)
+    
+
+
